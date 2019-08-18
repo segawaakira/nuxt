119ced56200d6492d342252efe75687559e880c2
segawa <>
2019-08-19 08:41:54 +0900

    ミューテーション使ってみる

--- nuxt-vuex/nuxt-vuex/pages/index.vue
+++ nuxt-vuex/nuxt-vuex/pages/index.vue
@@ -2,13 +2,14 @@
   <div class="container">
     <div>
       <p>{{ $store.state.message }}</p>
+      <button v-on:click="$store.commit('updateMessage')">
+        update
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
 }
 </script>
