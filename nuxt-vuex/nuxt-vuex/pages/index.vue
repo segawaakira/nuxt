c80be248742d9ab014480a9b45550844137ef22d
segawa <>
2019-08-19 08:57:08 +0900

    アクション経由で、ミューテーション操作しstate操作

--- nuxt-vuex/nuxt-vuex/pages/index.vue
+++ nuxt-vuex/nuxt-vuex/pages/index.vue
@@ -3,7 +3,9 @@
     <div>
       <p>{{ $store.state.message }}</p>
       <!-- <button v-on:click="$store.commit('updateMessage')"> -->
-      <button v-on:click="$store.commit('updateMessage','commit with payload')">
+      <!-- <button v-on:click="$store.commit('updateMessage','commit with payload')"> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')"> -->
+      <button v-on:click="$store.dispatch('updateMessageAction','Dispatch with payload')">
         update
       </button>
     </div>
