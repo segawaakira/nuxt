79cc446e0ef2ce92c2afea8b8ac78cf5296d70cf
segawa <>
2019-08-19 20:04:20 +0900

    momentで、時刻フォーマット

--- nuxt-todo/nuxt-todo/pages/todos.vue
+++ nuxt-todo/nuxt-todo/pages/todos.vue
@@ -3,14 +3,16 @@
     <!-- {{ todos }} -->
     <ul>
       <li v-for="todo in todos" :key="todo.id">
-        <input
-        type="checkbox"
-        v-bind:checked="todo.done"
-        @change="toggle(todo)">
-        <span v-bind:class="{done:todo.done}">
-          {{ todo.name }}{{ todo.created.toDate() }}
+        <span v-if="todo.created">
+          <input
+          type="checkbox"
+          v-bind:checked="todo.done"
+          @change="toggle(todo)">
+          <span v-bind:class="{done:todo.done}">
+            {{ todo.name }}{{ todo.created.toDate() | dateFilter }}
+          </span>
+          <button v-on:click="remove(todo.id)">X</button>
         </span>
-        <button v-on:click="remove(todo.id)">X</button>
       </li>
     </ul>
     <div class="form">
@@ -23,6 +25,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   data: function() {
     return {
@@ -49,12 +52,17 @@ export default {
     todos() {
       return this.$store.state.todos.todos
     }
+  },
+  filters: {
+    dateFilter: function(date) {
+      return moment(date).format('YYYY/MM/DD HH:mm:ss')
+    }
   }
 }
 </script>
 
 <style>
-  li > span.done{
+  li > span > span.done{
     text-decoration: line-through;
   }
 </style>
