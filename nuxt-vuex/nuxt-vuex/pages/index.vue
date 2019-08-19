10033ff5a359c47f7e42b2896ddc08125ce98b7b
segawa <>
2019-08-19 09:08:12 +0900

    モジュールモードに書き換え

--- nuxt-vuex/nuxt-vuex/pages/index.vue
+++ nuxt-vuex/nuxt-vuex/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="container">
     <div>
-      <p>{{ $store.state.message }}</p>
+
+      <!-- ▼ クラシックモード -->
+      <!-- <p>{{ $store.state.message }}</p> -->
       <!-- <button v-on:click="$store.commit('updateMessage')"> -->
       <!-- <button v-on:click="$store.commit('updateMessage','commit with payload')"> -->
       <!-- <button v-on:click="$store.dispatch('updateMessageAction')"> -->
-      <button v-on:click="$store.dispatch('updateMessageAction','Dispatch with payload')">
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction','Dispatch with payload')">
+        update
+      </button> -->
+      <!-- ▲ クラシックモード -->
+
+      <!-- ▼ モジュールモード -->
+      <p>{{ $store.state.hello.message }}</p>
+      <!-- <button v-on:click="$store.commit('updateMessage')"> -->
+      <!-- <button v-on:click="$store.commit('updateMessage','commit with payload')"> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')"> -->
+      <button v-on:click="$store.dispatch('hello/updateMessageAction','Dispatch with payload')">
         update
       </button>
+      <!-- ▲ モジュールモード -->
     </div>
   </div>
 </template>
